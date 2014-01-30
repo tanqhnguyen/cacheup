892b917a084d4a6ddb7547f524c500bd2a766cbb
Tan Nguyen <laoshanlung@gmail.com>
2014-01-30 06:54:10 +0200

    Just some title for tests

--- tests/redis_tests.js
+++ tests/redis_tests.js
@@ -14,82 +14,84 @@ var getRedisClient = function(cache, key) {
 }
 
 module.exports = function(cache) {
-  beforeEach(function(done){
-    cache.clear();
-    setTimeout(done, 200);
-  });
+  describe('Redis tests', function(){
+    beforeEach(function(done){
+      cache.clear();
+      setTimeout(done, 200);
+    });
 
-  it('sets and makes sure that redis has data', function(done){
-    var self = this;
-    var obj = {"some": "data"};
-    var key = 'test';
-    
-    cache.set(key, obj).ensure(done).done(function(data){
-      getRedisClient(cache, key).get(key, function(error, result){
-        should(_.isEqual(obj, JSON.parse(result))).be.ok;
-      });
-    }, should.fail);
-  });
-
-  it('sets and makes sure that redis has correct ttl', function(done){
-    var self = this;
-    var obj = {"some": "data"};
-    var key = new Date().getTime();
-    
-    cache.set(key, obj).ensure(done).done(function(data){
-      getRedisClient(cache, key).ttl(key, function(error, ttl){
-        should(ttl).be.equal(cache.DEFAULTS.ttl);
-      });
-    }, should.fail);
-  });
-
-  it('dels and makes sure that the key is gone in redis', function(done){
-    var self = this;
-    var obj = {"some": "data"};
-    var key = new Date().getTime();
-    
-    cache.set(key, obj).done(function(data){
-      cache.del(key).ensure(done).done(function(){
-        getRedisClient(cache, key).exists(key, function(error, result){
-          should(result).not.be.ok;
+    it('sets and makes sure that redis has data', function(done){
+      var self = this;
+      var obj = {"some": "data"};
+      var key = 'test';
+      
+      cache.set(key, obj).ensure(done).done(function(data){
+        getRedisClient(cache, key).get(key, function(error, result){
+          should(_.isEqual(obj, JSON.parse(result))).be.ok;
         });
       }, should.fail);
-    }, should.fail);
-  });
+    });
 
-  it('gets and keeps ttl in redis', function(done){
-    var self = this;
+    it('sets and makes sure that redis has correct ttl', function(done){
+      var self = this;
+      var obj = {"some": "data"};
+      var key = new Date().getTime();
+      
+      cache.set(key, obj).ensure(done).done(function(data){
+        getRedisClient(cache, key).ttl(key, function(error, ttl){
+          should(ttl).be.equal(cache.DEFAULTS.ttl);
+        });
+      }, should.fail);
+    });
 
-    var obj = {"some": "data"};
-    var key = new Date().getTime();
-
-    cache.set(key, obj).then(function(){
-      setTimeout(function(){
-        cache.get(key).done(function(data){
-          getRedisClient(cache, key).ttl(key, function(error, ttl){
-            should(ttl).be.lessThan(cache.DEFAULTS.ttl);
-            done();
+    it('dels and makes sure that the key is gone in redis', function(done){
+      var self = this;
+      var obj = {"some": "data"};
+      var key = new Date().getTime();
+      
+      cache.set(key, obj).done(function(data){
+        cache.del(key).ensure(done).done(function(){
+          getRedisClient(cache, key).exists(key, function(error, result){
+            should(result).not.be.ok;
           });
         }, should.fail);
-      }, 1000);
-    })
-  });
+      }, should.fail);
+    });
 
-  it('gets and extends ttl in redis', function(done){
-    var self = this;
+    it('gets and keeps ttl in redis', function(done){
+      var self = this;
 
-    var obj = {"some": "data"};
-    var key = new Date().getTime();
+      var obj = {"some": "data"};
+      var key = new Date().getTime();
 
-    cache.set(key, obj).then(function(){
-      setTimeout(function(){
-        cache.get(key, {extendttl: true}).done(function(data){
-          getRedisClient(cache, key).ttl(key, function(error, ttl){
-            should(ttl).be.equal(cache.DEFAULTS.ttl);
-            done();
-          });
-        }, should.fail);
-      }, 1000);
-    })
+      cache.set(key, obj).then(function(){
+        setTimeout(function(){
+          cache.get(key).done(function(data){
+            getRedisClient(cache, key).ttl(key, function(error, ttl){
+              should(ttl).be.lessThan(cache.DEFAULTS.ttl);
+              done();
+            });
+          }, should.fail);
+        }, 1000);
+      })
+    });
+
+    it('gets and extends ttl in redis', function(done){
+      var self = this;
+
+      var obj = {"some": "data"};
+      var key = new Date().getTime();
+
+      cache.set(key, obj).then(function(){
+        setTimeout(function(){
+          cache.get(key, {extendttl: true}).done(function(data){
+            getRedisClient(cache, key).ttl(key, function(error, ttl){
+              should(ttl).be.equal(cache.DEFAULTS.ttl);
+              done();
+            });
+          }, should.fail);
+        }, 1000);
+      })
+    });
   });
 }
